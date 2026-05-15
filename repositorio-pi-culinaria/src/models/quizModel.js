9a2cd7416ecff4e7751036f258e6aa927cdcaa8b
kajio-leo <leonardo.lasso@sptech.school>
2026-05-15 18:18:13 -0300

    Subindo quiz completamente conectado com bd

--- repositorio-pi-culinaria/src/models/quizModel.js
+++ repositorio-pi-culinaria/src/models/quizModel.js
@@ -3,16 +3,17 @@ var database = require("../database/config")
 function guardar(respostass, idUser) {
     let i = 0;
     console.log("ACESSEI O QUIZ MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function guardar():", respostass);
-    
+    let promises = [];
+
     while (i < respostass.length) {
         var instrucaoSql = `
             INSERT INTO resposta (idUsuario, idPergunta, alternativa, resposta) VALUES ('${idUser}', '${(i + 1)}', '${respostass[i].alternativa}', '${respostass[i].resposta}');
         `;
         console.log("Executando a instrução SQL: \n" + instrucaoSql);
         i++
-        // return database.executar(instrucaoSql); tentei tirar pra fora do while e so insere 1 dado 
+        promises.push(database.executar(instrucaoSql));
     }
-    return database.executar(instrucaoSql);
+    return Promise.all(promises);
 }
 
 function temnobanco(idUser) {
