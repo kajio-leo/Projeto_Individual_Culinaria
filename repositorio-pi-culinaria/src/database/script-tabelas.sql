97aa2d5f12233d891d47f1d75852a256fc8c1325
kajio-leo <leonardo.lasso@sptech.school>
2026-05-20 21:27:15 -0300

    Adicionando algumas coisas no css da dashboard, criando views das kpis que comporão a dashboard e preparando para puxar do banco de dados para gerar as kpis e os gráficos, além de "concluir" a seção "receitas favoritas" do perfil

--- repositorio-pi-culinaria/src/database/script-tabelas.sql
+++ repositorio-pi-culinaria/src/database/script-tabelas.sql
@@ -1,64 +1,189 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
+CREATE DATABASE projetoindividual;
+USE projetoindividual;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(80),
+email VARCHAR(321),
+senha VARCHAR(30));
+
+CREATE TABLE receita (
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(60),
+descricao VARCHAR(100),
+preparo VARCHAR(2000),
+tempoPreparoMin INT
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE ingrediente (
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(60)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE unidadeMed (
+id INT PRIMARY KEY AUTO_INCREMENT,
+tipo VARCHAR(30)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+CREATE TABLE receitaIngrediente (
+id INT AUTO_INCREMENT,
+idReceita INT,
+idIngrediente INT, 
+quantidade VARCHAR(20),
+unidadeMed INT,
+CONSTRAINT fkRecReceitaIngrediente FOREIGN KEY (idReceita) REFERENCES receita(id),
+CONSTRAINT fkIngrReceitaIngrediente FOREIGN KEY (idIngrediente) REFERENCES ingrediente(id),
+CONSTRAINT fkUnidReceitaIngrediente FOREIGN KEY (unidadeMed) REFERENCES unidadeMed(id),
+CONSTRAINT pkCompReceitaIngrediente PRIMARY KEY (id, idReceita, idIngrediente) 
 );
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+CREATE TABLE pergunta (
+id INT PRIMARY KEY,
+pergunta VARCHAR(200),
+alternativaA VARCHAR(30),
+alternativaB VARCHAR(30),
+alternativaC VARCHAR(30),
+alternativaD VARCHAR(30),
+alternativaE VARCHAR(30)
+);
 
-ALTER TABLE usuario ADD COLUMN cpf CHAR(11) UNIQUE;
+CREATE TABLE favorita (
+id INT AUTO_INCREMENT,
+idUsuario INT,
+idReceita INT,
+dtFavorita DATETIME DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT fkIdUserFav FOREIGN KEY (idUsuario) REFERENCES usuario(id),
+CONSTRAINT fkIdReceitaFav FOREIGN KEY (idReceita) REFERENCES receita(id),
+CONSTRAINT pkCompostaFav PRIMARY KEY (id, idUsuario, idReceita)
+);
+
+CREATE TABLE resposta (
+id INT AUTO_INCREMENT,
+idUsuario INT,
+idPergunta INT,
+alternativa VARCHAR(20),
+resposta VARCHAR(50),
+data_resposta DATETIME DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT fkIdUserResp FOREIGN KEY (idUsuario) REFERENCES usuario(id),
+CONSTRAINT fkIdPerguntaResp FOREIGN KEY (idPergunta) REFERENCES pergunta(id),
+CONSTRAINT pkCompostaResp PRIMARY KEY (id, idUsuario, idPergunta)
+);
+
+INSERT INTO pergunta VALUES 
+	(1, 'O que você prefere?', 'Doce', 'Salgado', null, null, null),
+    (2, 'Qual seu bolo favorito?', 'Cenoura', 'Morango', 'Limão', 'Chocolate', 'Fubá'),
+    (3, 'Qual seu docinho de festa preferido?', 'Brigadeiro', 'Beijinho', 'Bicho de Pé(de morango)', 'Cajuzinho', 'Bala de coco'),
+    (4, 'Qual sagado é mais gostoso?', 'Coxinha', 'Quibe', 'Esfiha', 'Risole', 'Bauru(joelho)'),
+    (5, 'O que é melhor?', 'Feijoada', 'Parmeggiana', 'Strogonoff', 'Virado à Paulista', 'Lasanha'),
+    (6, 'Qual ambiente você mais gosta?', 'Padaria', 'Cafeteria', 'Restaurante', 'Boteco', 'Feira'),
+    (7, 'Como prefere celebrar momentos especiais?', 'Restaurante', 'Karaokê', 'Pubs(bares) com música ao vivo', 'Confraternização em casa', 'Sozinho'),
+    (8, 'Com que frequência	você se reúne para comer com sua família (pais, avós, avôs, tios e tias)?', '4 ou mais vezes por mês', '2-3 vezes por mês', '1 vez por mês', '1 vez a cada três meses', '1 vez por ano'),
+    (9, 'Com que frequência você divide a mesa com sua família que mora com você?', 'Todo dia', 'Dia sim, dia não', '3 vezes por semana', 'De vez em quando', 'Nunca'),
+    (10, 'Gostaria de ver receitas incríveis para fazer e comer em família?', 'Simmm!', 'Não!', null, null, null);
+    
+INSERT INTO unidadeMed (tipo) VALUES
+	('g'),
+    ('mg'),
+    ('kg'),
+    ('ml'),
+    ('l'),
+	('xícara'),
+    ('xícaras'),
+	('colher de sopa'),
+	('colheres de sopa'),
+	('colher de sobremesa'),
+	('colheres de sobremesa'),
+	('colher de chá'),
+	('colheres de chá'),
+	('colher de café'),
+	('colheres de café'),
+	('pitada'),
+	('pitadas'),
+    ('fio'),
+    ('gota'),
+    ('gotas'),
+    ('dente'),
+    ('maço'),
+    ('a gosto'),
+    ('unidade');
+    
+INSERT INTO ingrediente (nome) VALUES
+	('manteiga'),
+	('açúcar refinado'),
+	('açúcar mascavo'),
+	('farinha'),
+	('bicarbonato'),
+	('fermento'),
+	('extrato de baunilha'),
+	('chocolate 70%'),
+	('chocolate ao leite'),
+	('ovo'),
+	('ovos'),
+    ('Arroz'),
+    ('Arroz japonês'),
+    ('Costela de Porco'),
+    ('Vagem'),
+    ('Berinjela'),
+    ('Molho Shoyu'),
+    ('Refrigerante saber Cola'),
+    ('Amido de milho'),
+	('Gengibre'),
+	('Cebolinha');
+
+INSERT INTO receita (nome, descricao, preparo, tempoPreparoMin) VALUES
+	('Cookies', 'Biscoito do tipo cookie com gotas de chocolate', 'Dourar a manteiga, resfriar ela até solidificar, bater com o açúcar mascavo e o açúcar refinado, até ficar 
+    mais clara, adicionar os ovos e o extrato de baunilha e mexer, adicionar a farinha, o bicarbonato e o fermento e incorporar os secos. Picar os chocolates em pedaços
+    menores e adicionar à massa, fazer o formato desejado e deixar na geladeira por no mínimo quatro horas. Se desejar pode congelar, a massa congelada tende a manter mais
+    a forma durante o assamento. Assar à 180 graus Celsius até ficar dourado', 120),
+    ('Bolo de Cenoura', 'Bolo de cenoura fofinho com cobertura de chocolate', null, 60),
+    ('Mousse de Chocolate', 'Mousse de chocolate aerado', null, 45),
+    ('Bolo de Limão', 'Bolo de limão fofinho com calda', null, 60),
+    ('Torta de Maçã', 'Torta de maçã com açúcar mascavo e canela', null, 90),
+    ('Salada de Frutas', 'Salada de frutas com mel', null, 30),
+    ('Costelinha Oriental', 'Costela de porco com berinjela e vagem em um molho agridoce', 'Lavar o arroz japonês e colocar na panela junto com a quantidade de água
+    indicada no saco do arroz. Cortar a costelinha em tiras, temperar com sal e pimenta do reino, picar a berinjela
+    em tiras do comprimento de um dedo, limpar as vagens, ralar o gengibre. Colocar um fio de azeite na panela e selar a costelinha até dourar, depois de dourar adicionar o molho
+    de soja e o refrigerante de cola, deixar ferver e depois de fervido adicionar a vagem e fechar a panela, depois de 5 minutos adicionar a berinjela e deixar cozinhar por mais
+    10 minutos. Separar uma parte do molho e dissolver o amido de milho nele, depois de dissolvido incorporar ao restante do molho e da carne na panela. Picar cebolinha
+    e servir junto com o arroz e a carne.', 90),
+    ('Frango com Quiabo', 'Frango suculento com quiabo', null, 60),
+    ('Hot Dog', 'Cachorro quente com purê de batatas', null, 60),
+    ('Strogonoff de Frango', 'Strogonoff de frango cremoso', null, 60),
+    ('Macarrão à Bolonhesa', 'Macarrão à Bolonhesa', null, 40),
+    ('Coxinha', 'Coxinha de frango com requeijão cremoso', null, 90);
+    
+    
+INSERT INTO receitaIngrediente (idReceita, idIngrediente, quantidade, unidadeMed) VALUES
+	(1, 1, '225', 1),
+    (1, 2, '150', 1),
+    (1, 3, '250', 1), 
+    (1, 4, '350', 1), 
+    (1, 5, '1/2', 12), 
+    (1, 6, '1', 12),
+    (1, 7, '1', 14),
+    (1, 8, '240', 1),
+    (1, 9, '170', 1),
+    (1, 11, '2', 24);
+
+CREATE VIEW vw_card AS SELECT f.idUsuario AS id, f.idReceita AS idRec, r.nome AS nome, r.descricao AS descr FROM receita AS r
+JOIN favorita AS f ON f.idReceita = r.id
+ORDER BY f.dtFavorita DESC;
+
+SELECT idRec, nome, descr FROM vw_card WHERE id = 1;
+
+CREATE VIEW vw_kpi1 AS 
+SELECT r.resposta AS opcao, 
+(SELECT COUNT(r.idPergunta) FROM resposta AS r WHERE idPergunta = 1) AS qtdTotal,
+(SELECT COUNT(r.idPergunta) FROM resposta AS r WHERE resposta = opcao) AS qtdIgual 
+FROM resposta AS r WHERE idUsuario = 3 AND idPergunta = 1;
+
+SELECT * FROM vw_kpi1;
+
+CREATE VIEW vw_kpi2 AS
+SELECT (SELECT f.idReceita FROM favorita AS f WHERE f.idReceita IN (SELECT idReceita FROM favorita WHERE idUsuario = 1) GROUP BY f.idReceita ORDER BY COUNT(f.idReceita) LIMIT 1) AS menosPopular,
+(SELECT COUNT(f.idUsuario) FROM favorita AS f WHERE f.idReceita = 
+(SELECT f2.idReceita FROM favorita AS f2 WHERE f2.idReceita IN (SELECT idReceita FROM favorita WHERE idUsuario = 1) GROUP BY f2.idReceita ORDER BY COUNT(f2.idReceita) LIMIT 1)) AS qtdUser,
+(SELECT COUNT(id) FROM usuario) AS qtdUserTot;
+
+SELECT * FROM vw_kpi2; 
+
